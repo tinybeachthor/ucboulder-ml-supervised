949c96ee80748a52dacc90c79f4eba9f800e0a16
Martin Toman <toman.martin@live.com>
2024-04-28 14:12:05 -0700

    Nix flake init

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ ];
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          git
+        ];
+      };
+    });
+}
